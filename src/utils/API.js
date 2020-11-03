abe6d5ca4891597f42b730d0b41ce19c29ae1e00
Jon Phoenix <jonathan.martb@gmail.com>
2020-11-02 16:38:04 -0800

    Adds API file

---
+++ src/utils/API.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+// Getting all the employees
+export default {
+    getEmployees: function() {
+        return axios.get('https://randomuser.me/api/?inc=name,email,dob,picture,phone&nat=us&results=50');
+    }
+};
