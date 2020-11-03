0b4573d67c0dc8b44261ea97a5e3b809082ce0a6
Jon Phoenix <jonathan.martb@gmail.com>
2020-11-02 19:26:43 -0800

    Adds sorting employees by name

--- src/components/EmployeesTable.jsx
+++ src/components/EmployeesTable.jsx
@@ -33,7 +33,8 @@ class EmployeesTable extends Component {
                     <tr>
                         <th scope='col'>#</th>
                         <th scope='col'>Image</th>
-                        <th scope='col'>Name</th>
+                        <th scope='col' className="click-me" title="Sort Alphabetically" 
+                            onClick={() => this.props.sortAlpha(this.props.employees)}>Name {this.props.sortArrow}</th>
                         <th scope='col'>Phone</th>
                         <th scope='col'>Email</th>
                         <th scope='col'>DOB</th>
