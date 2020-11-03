480ae1fff8534ec7a5efa689239352b534cf3f78
Jon Phoenix <jonathan.martb@gmail.com>
2020-11-02 19:53:26 -0800

    Final review

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 // Getting all the employees
 export default {
     getEmployees: function() {
-        return axios.get('https://randomuser.me/api/?inc=name,email,dob,picture,phone&nat=us&results=50');
+        return axios.get('https://randomuser.me/api/?results=200&nat=us');
     }
 };
