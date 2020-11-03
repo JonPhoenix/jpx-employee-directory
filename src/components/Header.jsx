9b4bc605d00ebb3e67472670dca61032bd6e71c8
Jon Phoenix <jonathan.martb@gmail.com>
2020-11-02 17:12:03 -0800

    Updates Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../css/styles.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <header className='jumbotron jumbotron-fluid'>
+                <div className='container'>
+                    <h3 className='display-4'>Employee Directory</h3>
+                    <p className='lead'>Search for an employee and sort the employees by name.</p>
+                </div>
+            </header>
+        );
+    }
+
+}
+
+export default Header;
---
+++ src/components/MainDirectory.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Header from './Header';
+// import Search from './Search';
+// import EmployeesTable from './EmployeesTable';
+// import API from '../utils/API';
+
+class MainDirectory extends Component {
+
+    render() {
+        return (
+            <>
+                < Header />
+            </>
+        );
+    };
+};
+
+export default MainDirectory;
